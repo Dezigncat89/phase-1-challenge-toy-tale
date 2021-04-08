f6a093bbcc9e2a528bb56c46abbfd5c1477142e1
Elaina Sorenson <78282104+Dezigncat89@users.noreply.github.com>
2021-04-08 10:47:50 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,3 +13,59 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+<div class="card">
+    <h2>Woody</h2>
+    <img src="[toy_image_url]" class="toy-avatar" />
+    <p>4 Likes </p>
+    <button class="like-btn" id="[toy_id]">Like <3</button>
+  </div>
+
+<div class="card">
+    <h2>Buzz Lightyear</h2>
+    <img src="[toy_image_url]" class="toy-avatar" />
+    <p>4 Likes </p>
+    <button class="like-btn" id="[toy_id]">Like <3</button>
+  </div>
+
+<div class="card">
+    <h2>Mr. Potato Head</h2>
+    <img src="[toy_image_url]" class="toy-avatar" />
+    <p>4 Likes </p>
+    <button class="like-btn" id="[toy_id]">Like <3</button>
+  </div>
+
+<div class="card">
+    <h2>Slinky Dog</h2>
+    <img src="[toy_image_url]" class="toy-avatar" />
+    <p>4 Likes </p>
+    <button class="like-btn" id="[toy_id]">Like <3</button>
+  </div>
+
+<div class="card">
+    <h2>Ham</h2>
+    <img src="[toy_image_url]" class="toy-avatar" />
+    <p>4 Likes </p>
+    <button class="like-btn" id="[toy_id]">Like <3</button>
+  </div>
+headers: 
+{
+  "Content-Type": "application/json",
+  Accept: "application/json"
+}
+
+body: JSON.stringify({
+  "name": "Jessie",
+  "image": "https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+  "likes": 0
+});
+
+headers: 
+{
+  "Content-Type": "application/json",
+  Accept: "application/json"
+}
+
+body: JSON.stringify({
+  "likes": <new number>
+});
